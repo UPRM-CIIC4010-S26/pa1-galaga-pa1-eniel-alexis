fcee8c78a52763dc9b991e01f2627191713b6c6e
Alexis-Arana <alexis.arana@upr.edu>
2026-03-13 23:06:04 -0400

    Pahse 2: Respawn Difficulty added

--- src/Program.cpp
+++ src/Program.cpp
@@ -99,7 +99,7 @@ void Program::ManageEnemyRespawns() {
 
     respawnCooldown -= 1;
     if (respawnCooldown <= 0) {
-        respawnCooldown = 1080;
+         respawnCooldown = std::max(1080 - (Enemy::score / 10), 300);
         for (std::pair<std::pair<float, float>, Enemy*>& p : Enemy::enemies) {
             if (!p.second && p.first.second != 150) {
                 int eType = GetRandomValue(1, 3);
